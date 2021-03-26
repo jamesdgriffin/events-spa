e8bed499842d6c491ae6b5bcb4642a81a80e6610
griffinjames1 <griffin.jame@northeastern.edu>
2021-03-26 05:29:14 -0400

    added redux

--- web-ui/src/App.js
+++ web-ui/src/App.js
@@ -1,26 +1,11 @@
-import { useState, useEffect } from 'react';
+import { Container } from 'react-bootstrap';
 import "./App.scss";
 import Users from "./Users";
-import { Container } from 'react-bootstrap';
-
-async function fetchUsers() {
-  let text = await fetch("http://localhost:4000/api/v1/users", {});
-  let resp = await text.json();
-  return resp.data;
-}
 
 function App() {
-  const [users, setUsers] = useState([]);
-
-  useEffect(() => {
-    if (users.length === 0) {
-      fetchUsers().then((xs) => setUsers(xs));
-    }
-  }, [users]);
-
   return (
     <Container>
-      <Users users={users} />
+      <Users />
     </Container>
   );
 }
